835c32baaaa0bc1e41fc87c48d55306dfa984d9b
EliotTarlov <tarlove@reed.edu>
2022-09-11 23:52:16 -0700

    curious error when compiling, functions don't know things exist outside of their body?

--- src/App.js
+++ src/App.js
@@ -15,9 +15,27 @@ async function scrape(URL) {
 }
 function App() {
   return (
-     <h1>
-     hello
-     </h1>
+    <div>
+        <meta charset="UTF-8"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge"></meta>
+        <title>Draft Yeilds</title>
+        <link rel="stylesheet" href="css/style.css"></link>
+        <script src="basis.js"> </script>
+        <script src="node_modules/pupeteer/package.json"></script>
+        <script>
+          const req= 1;
+          function print_request(){
+            console.log(req)}
+        </script>
+       
+        <h1>
+            Please enter 17 lands url.
+        </h1>
+        <button onclick="print_request()">
+        </button>
+    </div>    
+    
   );
 
 };
